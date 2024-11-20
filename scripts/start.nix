8fccaaa57308a16d11c5672de29ae80e6c28bb81
mboyea <contact@mboyea.com>
2024-11-20 09:49:25 -0600

    Move Lacuna help function into base directory

--- scripts/start.nix
+++ scripts/start.nix
@@ -7,13 +7,83 @@
   # authServer,
   # trackingServer,
   cliArgs ? []
-}: pkgs.writeShellApplication {
-  name = "${name}-start-${version}";
-  runtimeInputs = [
-    webServer
-  ];
-  text = ''
-    set -- "$@" ${pkgs.lib.strings.concatStringsSep " " cliArgs}
-    ${pkgs.lib.getExe webServer} "$@"
-  '';
-}
+}: let
+  start = rec {
+    help = pkgs.writeShellApplication {
+      name = "${name}-start-help-${version}";
+      text = ''
+        echo "Start the app locally."
+        echo
+        echo "Usage:"
+        echo "  nix run .#start [SCRIPT]"
+        echo
+        echo "Scripts:"
+        echo "  help  --help  -h  Print this helpful information"
+        echo "  dev               Run all servers from their source code with hot-reloading where possible, and without Docker where possible"
+        echo "  preview           Build the app, then run the build results without Docker where possible"
+        echo "  container         Build the app, package it into Docker containers, then run the docker containers"
+      '';
+    };
+    # TODO implement dev script
+    dev = pkgs.writeShellApplication {
+      name = "${name}-start-dev-${version}";
+      runtimeInputs = [
+        webServer
+      ];
+      text = ''
+        ${pkgs.lib.getExe webServer} "$@"
+      '';
+    };
+    # TODO implement preview script
+    preview = pkgs.writeShellApplication {
+      name = "${name}-start-preview-${version}";
+      runtimeInputs = [
+        webServer
+      ];
+      text = ''
+        ${pkgs.lib.getExe webServer} "$@"
+      '';
+    };
+    main = pkgs.writeShellApplication {
+      name = "${name}-start-main-${version}";
+      runtimeInputs = [
+        help
+      ];
+      text = ''
+        set -- "$@" ${pkgs.lib.strings.concatStringsSep " " cliArgs}
+
+        echo_error() {
+          echo "Error in start-main:" "$@" 1>&2;
+        }
+
+        interpret_args() {
+          while [[ $# -gt 0 ]]; do
+            case $1 in
+              help|--help|-h)
+                : "''${script:="${pkgs.lib.getExe help}"}"
+                shift
+              ;;
+              *)
+                additional_args+=("$1")
+                shift
+              ;;
+            esac
+          done
+          set -- "''${additional_args[@]}"
+        }
+
+        main() {
+          interpret_args "$@"
+          if [[ -n "''${script:-}" ]]; then
+            "$script" "''${additional_args[@]}"
+          else
+            echo_error "No valid script identified among arguments:" "''${additional_args[@]}"
+            exit 1
+          fi
+        }
+
+        main "$@"
+      '';
+    };
+  };
+in start.main
--- sveltekit/scripts/start.nix
+++ sveltekit/scripts/start.nix
@@ -4,23 +4,6 @@
   version,
 }: let
   start = rec {
-    # TODO: move help script out into lacuna/scripts/start.nix
-    help = pkgs.writeShellApplication {
-      name = "${name}-start-help-${version}";
-      runtimeInputs = [];
-      text = ''
-        echo "Start the app locally."
-        echo
-        echo "Usage:"
-        echo "  nix run .#start [SCRIPT]"
-        echo
-        echo "Scripts:"
-        echo "  help  --help  -h  Print this helpful information"
-        echo "  dev               Run all servers from their source code with hot-reloading where possible, and without Docker where possible"
-        echo "  preview           Build the app, then run the build results without Docker where possible"
-        echo "  container         Build the app, package it into Docker containers, then run the docker containers"
-      '';
-    };
     # TODO implement dev and preview scripts
     dev = null;
     preview = null;
@@ -30,16 +13,12 @@
         help
       ];
       text = ''
-        echoerror() {
+        echo_error() {
           echo "Error in start-main:" "$@" 1>&2;
         }
         interpret_args() {
           while [[ $# -gt 0 ]]; do
             case $1 in
-              help|--help|-h)
-                : "''${script:="${pkgs.lib.getExe help}"}"
-                shift
-              ;;
               *)
                 additional_args+=("$1")
                 shift
@@ -53,7 +32,7 @@
           if [[ -n "''${script:-}" ]]; then
             "$script" "''${additional_args[@]}"
           else
-            echoerror "No valid script identified among arguments:" "''${additional_args[@]}"
+            echo_error "No valid script identified among arguments:" "''${additional_args[@]}"
             exit 1
           fi
         }
