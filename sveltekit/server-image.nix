7c4c7ed400856f4e43eb20e5a324526d24330912
mboyea <contact@mboyea.com>
2024-12-02 13:12:54 -0600

    implemented baseImage exapmle

--- sveltekit/server-image.nix
+++ sveltekit/server-image.nix
@@ -2,6 +2,20 @@
   name = "${name}-server-image";
   tag = version;
   baseImage = null;
+  # EXAMPLE:
+  # pkgs.dockerTools.pullImage {
+  #   # variables to update base image found using:
+  #   # xdg-open https://hub.docker.com/_/busybox/tags
+  #   # nix develop
+  #   # nix-prefetch-docker --quiet --image-name busybox --image-tag 1.37.0 --image-digest sha256:bd39d7ac3f02301aec35d27a633d643770c0c4073c5b8cb588b1680c4f1e84e5
+  #   imageName = "busybox";
+  #   imageDigest ="sha256:bd39d7ac3f02301aec35d27a633d643770c0c4073c5b8cb588b1680c4f1e84e5";
+  #   finalImageName = "busybox";
+  #   finalImageTag = "1.37.0";
+  #   sha256 = "1rwnka21y7rj8jz250ay7c6mmyja7nngl0ia1z64rbcxq4ylgdvi";
+  #   os = "linux";
+  #   arch = "amd64";
+  # };
 in {
   inherit name version tag;
   stream = pkgs.dockerTools.streamLayeredImage {
