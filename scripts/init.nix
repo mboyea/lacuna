428097ddf297ed89f332b959d8706c74dc9c5ce0
mboyea <contact@mboyea.com>
2024-12-16 12:44:45 -0600

    draft deploy help and init help

---
+++ scripts/init.nix
@@ -0,0 +1,13 @@
+# #### TODO `nix run .#init help`
+# 
+# ```sh
+# Initialize the app for deployment.
+# 
+# Usage:
+#   nix run .#init [SCRIPT]
+# 
+# Scripts:
+#   help  --help  -h  Print this helpful information
+#   stage             Stand up all hosting providers for a staging deployment
+#   prod              Stand up all hosting providers for a production deployment
+# ```
