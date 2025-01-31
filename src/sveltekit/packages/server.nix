b3765ac0b4e61f29a55ec6bec13591b7d2bd7c2d
mboyea <contact@mboyea.com>
2025-01-30 19:06:51 -0600

    set name of sveltekit webserver to www-example-com

--- src/sveltekit/packages/server.nix
+++ src/sveltekit/packages/server.nix
@@ -10,7 +10,7 @@
   # Generate a new hash using:
   #   nix-shell -p prefetch-npm-deps
   #   prefetch-npm-deps path/to/sveltekit/package-lock.json
-  npmDepsHash = "sha256-XwHQc8NkoiWmCW4g7bBis+m1lC7IunRyzoKFe+SrLfQ=";
+  npmDepsHash = "sha256-c2LJSHc3J+/WDxnSaBbbMEIu2VhWlwYYSSN7Rwy5MSM=";
   npmBuildScript = "build";
   installPhase = ''
     mkdir -p $out/bin $out/lib
