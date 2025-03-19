9f9befd689c9d6bafb8ae63f87471d0e7b4460f0
mboyea <contact@mboyea.com>
2025-03-19 16:15:51 -0500

    implement sticky header/footer

--- modules/sveltekit/src/routes/+layout.svelte
+++ modules/sveltekit/src/routes/+layout.svelte
@@ -2,4 +2,62 @@
   import './+layout.scss'
 </script>
 
-<slot />
+<header id="page-header" class="container">
+  <hgroup>
+    <h1>Pico</h1>
+    <p>A pure HTML example, without dependencies.</p>
+  </hgroup>
+  <nav>
+    <ul>
+      <li>
+        <details class="dropdown">
+          <summary role="button" class="secondary">Theme</summary>
+          <ul>
+            <li><a href="#" data-theme-switcher="auto">Auto</a></li>
+            <li><a href="#" data-theme-switcher="light">Light</a></li>
+            <li><a href="#" data-theme-switcher="dark">Dark</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </nav>
+</header>
+<main tabindex="-1" id="page-main" class="container">
+  <slot />
+</main>
+<footer id="page-footer" class="container">
+  <small
+    >Built with <a href="https://picocss.com">Pico</a> •
+    <a href="https://github.com/picocss/examples/blob/master/v2-html/index.html"
+      >Source code</a
+    ></small
+  >
+</footer>
+
+<style lang="scss">
+  :global(html, body) {
+    height: max(100%, 100vh);
+  }
+  :global(body) {
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+  }
+  header {
+    flex: 0 0;
+    z-index: 1;
+  }
+  main {
+    flex: 1 0;
+    display: flex;
+    flex-direction: column;
+    &:focus {
+      outline: none;
+    }
+  }
+  footer {
+    flex: 0 0;
+    z-index: 1;
+  }
+</style>
+
